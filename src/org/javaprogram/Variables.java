282c6bd9a4df76450a3a7d7e936e72c1f368a422
ThivakarDeivendran <dthivakar1@gmail.com>
2023-07-30 18:46:50 +0530

    Update JavaProgram

--- src/org/javaprogram/Variables.java
+++ src/org/javaprogram/Variables.java
@@ -1,4 +1,4 @@
-//package org.javaprogram;
+package org.javaprogram;
 //
 //public class Variables {
 //	
@@ -41,3 +41,218 @@
 //	}
 //
 //}
+import java.io.*;
+import java.util.*;
+
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
+public class Variables {
+//    public static void main(String[] args) {
+//        ArrayList<String> list = new ArrayList<>();
+//        list.add("Alice");
+//        list.add(Boolean.TRUE.toString());
+//        list.add("Bob");
+//
+//        System.out.println(list);
+//    }
+	
+	
+//	public class Main {
+//	    public static void main(String[] args) throws IOException {
+//	        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//	        List<Integer> input = new ArrayList<>();
+//
+//	        String[] inputItems = bufferedReader.readLine().split(",");
+//	        for (String inputItem : inputItems) {
+//	            int number = Integer.parseInt(inputItem.trim());
+//	            input.add(number);
+//	        }
+//
+//	        int outcome = getSmallestPositiveInteger(input);
+//	        System.out.println(outcome);
+//
+//	        bufferedReader.close();
+//	    }
+//
+//	    public static int getSmallestPositiveInteger(List<Integer> input) {
+//	        Set<Integer> positiveSet = new HashSet<>();
+//
+//	        for (int num : input) {
+//	            if (num > 0) {
+//	                positiveSet.add(num);
+//	            }
+//	        }
+//
+//	        int smallestPositive = 1;
+//	        while (positiveSet.contains(smallestPositive)) {
+//	            smallestPositive++;
+//	        }
+//
+//	        return smallestPositive;
+//	    }
+//	
+	
+	
+	
+
+
+	//public class Main {
+//	    public static void main(String[] args) throws IOException {
+//	        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//	        String S = bufferedReader.readLine();
+//
+//	        int outcome = solution(S);
+//	        System.out.println(outcome);
+//
+//	        bufferedReader.close();
+//	    }
+//
+//	    public static int solution(String S) {
+//	        int maxSymmetricLength = 0;
+//	        int questionCount = 0;
+//	        int openCount = 0;
+//
+//	        for (char c : S.toCharArray()) {
+//	            if (c == '<') {
+//	                if (questionCount > 0) {
+//	                    openCount++;
+//	                    questionCount--;
+//	                } else {
+//	                    openCount++;
+//	                }
+//	            } else if (c == '>') {
+//	                if (questionCount > 0) {
+//	                    openCount--;
+//	                    questionCount--;
+//	                } else {
+//	                    openCount--;
+//	                }
+//	            } else if (c == '?') {
+//	                questionCount++;
+//	            }
+//
+//	            if (questionCount >= Math.abs(openCount)) {
+//	                maxSymmetricLength = Math.max(maxSymmetricLength, 2 * Math.abs(openCount));
+//	            }
+//	        }
+//
+//	        return maxSymmetricLength;
+//	    }
+	
+	
+	
+	
+
+	//public class Main {
+//	    public static void main(String[] args) throws IOException {
+//	    	
+//	    	        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//	    	        String input = bufferedReader.readLine();
+//
+//	    	        String[] inputItems = input.split(",");
+//	    	        int a = Integer.parseInt(inputItems[0].trim());
+//	    	        int b = Integer.parseInt(inputItems[1].trim());
+//
+//	    	        String outcome = getString(a, b);
+//	    	        System.out.println(outcome);
+//
+//	    	        bufferedReader.close();
+//	    	    }
+//
+//	    public static String getString(int a, int b) {
+//	        StringBuilder stringBuilder = new StringBuilder();
+//	        char prevChar = ' ';
+//	        int countA = 0;
+//	        int countB = 0;
+//
+//	        while (countA < a || countB < b) {
+//	            if (a > b && countA < a) {
+//	                stringBuilder.append("a");
+//	                countA++;
+//	                prevChar = 'a';
+//	            } else if (b > a && countB < b) {
+//	                stringBuilder.append("b");
+//	                countB++;
+//	                prevChar = 'b';
+//	            } else {
+//	                if (prevChar == 'a') {
+//	                    stringBuilder.append("b");
+//	                    countB++;
+//	                    prevChar = 'b';
+//	                } else {
+//	                    stringBuilder.append("a");
+//	                    countA++;
+//	                    prevChar = 'a';
+//	                }
+//	            }
+//	        }
+//
+//	        return stringBuilder.toString();
+//	    }
+//	
+
+	
+
+
+
+	    static List<Employee> removeDuplicates(List<Employee> employees) {
+	        // Using a LinkedHashMap to maintain insertion order and ensure uniqueness based on employeeId
+	        Map<Integer, Employee> employeeMap = new LinkedHashMap<>();
+	        
+	        for (Employee emp : employees) {
+	            employeeMap.putIfAbsent(emp.employeeId, emp);
+	        }
+	        
+	        return new ArrayList<>(employeeMap.values());
+	    }
+
+	    static class Employee {
+	        Integer employeeId;
+	        String employeeName;
+	        String department;
+
+	        public Employee(Integer employeeId, String employeeName, String department) {
+	            this.employeeId = employeeId;
+	            this.employeeName = employeeName;
+	            this.department = department;
+	        }
+	    }
+
+	    public static void main(String[] args) throws IOException {
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        PrintWriter wr = new PrintWriter(System.out);
+
+	        int n = Integer.parseInt(br.readLine().trim());
+
+	        List<Employee> employees = new ArrayList<>();
+	        for (int i = 0; i < n; i++) {
+	            String[] employeeDetails = br.readLine().split(" ");
+	            Employee e = new Employee(Integer.valueOf(employeeDetails[0]), employeeDetails[1], employeeDetails[2]);
+	            employees.add(e);
+	        }
+
+	        List<Employee> out_ = removeDuplicates(employees);
+	        for (int i = 0; i < out_.size(); i++) {
+	            System.out.print(out_.get(i).employeeId);
+	            System.out.print(" ");
+	            System.out.print(out_.get(i).employeeName);
+	            System.out.print(" ");
+	            System.out.print(out_.get(i).department);
+	            System.out.println("");
+	        }
+
+	        wr.close();
+	        br.close();
+	    }
+	}
+
+
+
